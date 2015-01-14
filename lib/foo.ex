0ea03c41f48e423673e2cdcb38b32c269efce793
Mark Haskamp <mark.haskamp@pinnsg.com>
2015-01-13 22:18:58 -0500

    closer

--- lib/foo.ex
+++ lib/foo.ex
@@ -1,10 +1,13 @@
 defmodule Foo do
   def init do
-    p  = spawn(Bar, :init, [{:a, 1}])
-    p2 = spawn(Bar, :init, [{:b, 101}])
+    p  = spawn(Bar, :init, [{:a, [1,2,3,4]}])
+    p2 = spawn(Bar, :init, [{:b, [4,3,2,1]}])
 
-    send p,  {:a, :ed}
-    send p2, {:b, :ed}
+    send p,  {:a, :top_card}
+    send p2, {:b, :top_card}
+
+    send p,  {:a, :top_card}
+    send p2, {:b, :top_card}
   end
 
 end
@@ -19,8 +22,13 @@ defmodule Bar do
 
   def receive_loop do
     receive do
-      {tag, :ed} -> Agent.get(tag, fn(x) -> IO.puts(x) end)
+      {tag, :top_card} -> 
+        card = Agent.get(tag, fn(cards) -> hd(cards) end)
+        Agent.update(tag, fn(cards) -> tl(cards) end)
+        IO.puts card
     end
+
+    receive_loop
 
   end
 end
--- lib/game.ex
+++ lib/game.ex
@@ -1,21 +1,40 @@
-# defmodule Game do
-#   def start do
-#     player1 = spawn(Player, :init, [[1,2,3,4]])
-# 
-#     send player1, :top_card
-#   end
-# end
-# 
-# 
-# defmodule Player do
-#   def init(cards) do
-#     Agent.start(fn -> cards end, name: Foo)
-#   end
-# 
-#   def top_card do
-#     top_card = Agent.get(Foo, &(hd(&1)))
-#     Agent.update(Foo, &(tl(&1)))
-#     IO.puts top_card
-#   end
-# end
-# 
+defmodule Dealer do
+  def init do
+    p1 = spawn(Player, :init, [{:p1, [1,2,3,4]}])
+    p2 = spawn(Player, :init, [{:p2, [5,6,7,8]}])
+
+    send p1, {self, :p1, :top_card}
+    send p2, {self, :p2, :top_card}
+
+    receive do
+      {pid1, :card, card} -> IO.puts "#{inspect pid1} played the #{card}"
+    end
+
+    receive do
+      {pid2, :card, card} -> IO.puts "#{inspect pid2} played the #{card}"
+    end
+  end
+end
+
+
+defmodule Player do
+
+  def init({tag, n}) do
+    Agent.start(fn -> n end, name: tag)
+    receive_loop
+  end
+
+  def receive_loop do
+    receive do
+      {pid, tag, :top_card} -> 
+        card = Agent.get(tag, fn(cards) -> hd(cards) end)
+        Agent.update(tag, fn(cards) -> tl(cards) end)
+        send pid, {self, :card, card}
+
+    end
+
+    receive_loop
+
+  end
+end
+
